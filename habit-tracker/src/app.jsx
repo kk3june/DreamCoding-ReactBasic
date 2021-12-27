8d37dd758ead9cd3649eba7850fca244be84fda0
3jun <kk3june@gmail.com>
2021-12-27 18:25:55 +0900

    Mod: app.js file format change to jsx and using js code in jsx(lecture 4.5)

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,7 +1,17 @@
+import React from "react";
 import "./app.css";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const name = "jun";
+  return (
+    <>
+      <h1>Hello :)</h1>
+      {name && <h1>Oh!! Hello! {name} </h1>}
+      {["📔", "📦"].map((item) => (
+        <h1>{item}</h1>
+      ))}
+    </>
+  );
 }
 
 export default App;
