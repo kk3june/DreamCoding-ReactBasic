dbc03829bf1d009bd3665db4a5ebcb03a22f9fcc
3jun <kk3june@gmail.com>
2021-12-28 09:04:37 +0900

    Mod: make habit component, install and use fontawesome(lecture 4.6)

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import "./app.css";
+import Habit from "./components/habit";
 
 function App() {
-  const name = "jun";
-  return (
-    <>
-      <h1>Hello :)</h1>
-      {name && <h1>Oh!! Hello! {name} </h1>}
-      {["📔", "📦"].map((item) => (
-        <h1>{item}</h1>
-      ))}
-    </>
-  );
+  return <Habit />;
 }
 
 export default App;
