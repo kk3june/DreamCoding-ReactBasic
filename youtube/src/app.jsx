7c6f019738f0ed56019e49c8712ec0247f94394f
3jun <kk3june@gmail.com>
2022-01-11 15:28:17 +0900

    Mod: show popular list using API(lecture 8.1~8.2)

---
+++ youtube/src/app.jsx
@@ -0,0 +1,26 @@
+import "./app.css";
+import React, { useState, useEffect } from "react";
+import VideoList from "./components/video_list/video_list";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyAYU2ijQFPPZ1sm_G-RLkdtWiWtwM70wro",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  });
+
+  return <VideoList videos={videos} />;
+}
+
+export default App;
