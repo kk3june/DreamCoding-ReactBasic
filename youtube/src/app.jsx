2356310c8f2b952891eaf433cc5944615c9106ef
3jun <kk3june@gmail.com>
2022-01-13 10:14:28 +0900

    Add: add search-bar only ui(lecture 8.5~8.6)

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -1,6 +1,7 @@
-import "./app.css";
+import "./app.module.css";
 import React, { useState, useEffect } from "react";
 import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -20,7 +21,12 @@ function App() {
       .catch((error) => console.log("error", error));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className="styles.app">
+      <SearchHeader />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
